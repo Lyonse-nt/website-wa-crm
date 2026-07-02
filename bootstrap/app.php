422657ba1fcb7f35741f668cead42e2041215609
Jonnint <jonni.nicorna.t@gmail.com>
2026-07-02 13:40:59 +0700

    Fix CSRF exemption and add route:clear for webhook

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->validateCsrfTokens(except: [
-            'webhook',
-            'webhook/*',
+            '/webhook',
+            '/webhook/*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
